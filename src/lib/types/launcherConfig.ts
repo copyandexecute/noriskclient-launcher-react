1e5c0e9af09b6f453b9de5ece347f3b2b049a1be
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2025-04-28 17:56:18 +0200

    Create launcherConfig.ts

---
+++ src/lib/types/launcherConfig.ts
@@ -0,0 +1,10 @@
+// This file is auto-generated from the Rust sources. Do not edit manually.
+// Corresponding Rust file: src-tauri/src/state/config_state.rs
+
+export interface LauncherConfig {
+  version: number; // u32
+  is_experimental: boolean;
+  auto_check_updates: boolean;
+  concurrent_downloads: number; // usize
+  enable_discord_presence: boolean;
+} 
